fee3c61fbe8c279e22b41cc1363940aaead1bfbb
TrinhTrungDung <dungtrungtrinh@gmail.com>
2022-04-01 10:20:45 +0700

    Remove junk file

--- test.js
+++
@@ -1,9 +0,0 @@
-const getURLParams = url => {
-    new Proxy(new URL(url), {
-        get: (params, prop) => params.get(prop),
-    });
-};
-
-const url = new URL('https://www.youtube.com/watch?v=YgZbWmfkB6Y');
-console.log(new URL('https://www.youtube.com/watch?v=YgZbWmfkB6Y').searchParams.get("v"));
-console.log(getURLParams('https://www.youtube.com/watch?v=YgZbWmfkB6Y'));
